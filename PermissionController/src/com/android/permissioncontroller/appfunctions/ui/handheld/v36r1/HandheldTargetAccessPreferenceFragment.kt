688b8be3bee49c76ae7d551d3c767bdf23324c40
Richard MacGregor <rmacgregor@google.com>
2025-06-02 15:50:32 -0700

    Implement initial structure for app functions
    This commit adds the initial structure for the app function agent
    screens. This is based on the design from DefaultAppActivity.
    
    LOW_COVERAGE_REASON= FLAG_NOT_ENABLED
    
    Relnote: N/A
    Bug: 419326017
    Flag: android.permission.flags.app_function_access_ui_enabled
    Test: atest CtsPermissionUiTestCases
    
    Change-Id: I3642be5cedd186ef0d861d32ddc1f8a9e9673ae2

---
+++ PermissionController/src/com/android/permissioncontroller/appfunctions/ui/handheld/v36r1/HandheldTargetAccessPreferenceFragment.kt
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.permissioncontroller.appfunctions.ui.handheld.v36r1
+
+import android.os.Bundle
+import androidx.preference.Preference
+import com.android.permissioncontroller.appfunctions.ui.handheld.v36r1.HandheldTargetAccessPreferenceFragment.Parent
+import com.android.permissioncontroller.appfunctions.ui.v36r1.TargetAccessChildFragment
+import com.android.settingslib.widget.SettingsBasePreferenceFragment
+
+/**
+ * Handheld preference fragment for the management of app function targets.
+ *
+ * Must be added as a child fragment and its parent fragment must implement [Parent].
+ */
+class HandheldTargetAccessPreferenceFragment(val targetPackage: String) :
+    SettingsBasePreferenceFragment(), TargetAccessChildFragment.Parent {
+    @Suppress("DEPRECATION")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            val fragment = TargetAccessChildFragment.newInstance(targetPackage)
+            childFragmentManager.beginTransaction().add(fragment, null).commit()
+        }
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        // Preferences will be added by the child fragment later.
+    }
+
+    override fun createPreference(): Preference {
+        return Preference(requireContext())
+    }
+
+    override fun setTitle(title: CharSequence) {
+        requireParent().setTitle(title)
+    }
+
+    override fun onPreferenceScreenChanged() {
+        requireParent().onPreferenceScreenChanged()
+    }
+
+    private fun requireParent(): Parent {
+        return requireParentFragment() as Parent
+    }
+
+    /** Interface that the parent fragment must implement. */
+    interface Parent {
+        /**
+         * Set the title of the current settings page.
+         *
+         * @param title the title of the current settings page
+         */
+        fun setTitle(title: CharSequence)
+
+        /**
+         * Callback when changes have been made to the {@link androidx.preference.PreferenceScreen}
+         * of this {@link PreferenceFragmentCompat}.
+         */
+        fun onPreferenceScreenChanged()
+    }
+
+    companion object {
+        /**
+         * Create a new instance of this fragment.
+         *
+         * @return a new instance of this fragment
+         */
+        @JvmStatic
+        fun newInstance(targetPackage: String): HandheldTargetAccessPreferenceFragment {
+            return HandheldTargetAccessPreferenceFragment(targetPackage)
+        }
+    }
+}
---
+++ PermissionController/src/com/android/permissioncontroller/appfunctions/ui/v36r1/AgentAccessChildFragment.kt
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.permissioncontroller.appfunctions.ui.v36r1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.android.permissioncontroller.R
+
+/**
+ * Child fragment for modifying agent access of target apps.
+ *
+ * <p>
+ * Must be added as a child fragment and its parent fragment must be a {@link
+ * PreferenceFragmentCompat} that implements {@link Parent}.
+ *
+ * @param <PF> type of the parent fragment
+ */
+class AgentAccessChildFragment<PF> : Fragment(), Preference.OnPreferenceClickListener where
+PF : PreferenceFragmentCompat,
+PF : AgentAccessChildFragment.Parent {
+    @Suppress("DEPRECATION")
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val preferenceFragment = requirePreferenceFragment()
+        preferenceFragment.setTitle(getString(R.string.app_function_access_settings_title))
+    }
+
+    override fun onPreferenceClick(preference: Preference): Boolean {
+        // TODO(rmacgregor): Implement onPreferenceClick
+        return true
+    }
+
+    private fun requirePreferenceFragment(): PF {
+        @Suppress("UNCHECKED_CAST")
+        return requireParentFragment() as PF
+    }
+
+    /** Interface that the parent fragment must implement. */
+    interface Parent {
+        /**
+         * Set the title of the current settings page.
+         *
+         * @param title the title of the current settings page
+         */
+        fun setTitle(title: CharSequence)
+
+        /** Creates a new preference for a target app. */
+        fun createPreference(): Preference
+
+        /**
+         * Callback when changes have been made to the {@link PreferenceScreen} of the parent {@link
+         * PreferenceFragmentCompat}.
+         */
+        fun onPreferenceScreenChanged()
+    }
+
+    companion object {
+        /**
+         * Create a new instance of AgentAccessChildFragment
+         *
+         * @return a new instance of AgentAccessChildFragment
+         */
+        fun newInstance(): AgentAccessChildFragment<*> = AgentAccessChildFragment<Nothing>()
+    }
+}
