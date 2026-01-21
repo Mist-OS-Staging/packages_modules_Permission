fc4fbc7237196204630afc2ff9189d0629a912cb
Yi-an Chen <theianchen@google.com>
2026-01-21 23:16:43 +0000

    Add Fragment and UI for the agent timline in dashboard
    Bug: 436261702
    Flag: com.android.permissioncontroller.flags.privacy_dashboard_agent_activity_enabled
    Low-Coverage-Reason: FLAG_NOT_ENABLED
    Test: Manual
    Relnote: Add Fragment and UI for the agent timline in dashboard
    Change-Id: I1f6e2acb51cf9fe955caa80e15d74273cca3f893

---
+++ PermissionController/src/com/android/permissioncontroller/appfunctions/ui/handheld/v37/AgentUsageDetailsFragment.kt
@@ -0,0 +1,291 @@
+/*
+ * Copyright (C) 2026 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.permissioncontroller.appfunctions.ui.handheld.v37
+
+import android.content.Intent
+import android.content.Intent.EXTRA_PACKAGE_NAME
+import android.os.Bundle
+import android.os.Process
+import android.text.format.DateFormat
+import android.util.Log
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import androidx.core.net.toUri
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.preference.Preference
+import androidx.preference.PreferenceCategory
+import androidx.preference.PreferenceScreen
+import com.android.permissioncontroller.R
+import com.android.permissioncontroller.appfunctions.ui.viewmodel.v37.AgentAccessUiInfo
+import com.android.permissioncontroller.appfunctions.ui.viewmodel.v37.AgentUsageDetailsUiState
+import com.android.permissioncontroller.appfunctions.ui.viewmodel.v37.AgentUsageDetailsViewModel
+import com.android.permissioncontroller.appfunctions.ui.viewmodel.v37.AgentUsageDetailsViewModelFactory
+import com.android.permissioncontroller.permission.ui.ManagePermissionsActivity.EXTRA_SHOW_7_DAYS
+import com.android.permissioncontroller.permission.ui.handheld.SettingsWithLargeHeader
+import com.android.permissioncontroller.permission.utils.KotlinUtils
+import java.time.Instant
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
+
+class AgentUsageDetailsFragment : SettingsWithLargeHeader() {
+    private lateinit var agentPackageName: String
+
+    private lateinit var viewModel: AgentUsageDetailsViewModel
+
+    private lateinit var show7DaysMenuItem: MenuItem
+    private lateinit var show24HoursMenuItem: MenuItem
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val argumentPackageName = arguments?.getString(EXTRA_PACKAGE_NAME)
+        if (argumentPackageName == null) {
+            Log.e(
+                LOG_TAG,
+                "No agent package name was provided. This is mandatory" +
+                    " for creating AgentUsageDetailsFragment",
+            )
+            requireActivity().finish()
+            return
+        }
+
+        agentPackageName = argumentPackageName
+        val factory =
+            AgentUsageDetailsViewModelFactory(requireActivity().application, agentPackageName)
+        viewModel = ViewModelProvider(this, factory)[AgentUsageDetailsViewModel::class.java]
+        viewModel.updateShow7DaysToggle(arguments?.getBoolean(EXTRA_SHOW_7_DAYS) ?: false)
+
+        val agentLabel =
+            KotlinUtils.getPackageLabel(
+                requireActivity().application,
+                agentPackageName,
+                Process.myUserHandle(),
+            )
+        val title = resources.getString(R.string.agent_activity_timeline_title, agentLabel)
+        requireActivity().setTitle(title)
+
+        requireActivity().actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.agentUsageDetailsUiDataFlow
+                    .flowOn(Dispatchers.Default)
+                    .collect(::onUiStateChanged)
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        requireActivity()
+            .addMenuProvider(
+                object : MenuProvider {
+                    override fun onCreateMenu(menu: Menu, inflator: MenuInflater) {
+                        show7DaysMenuItem =
+                            menu.add(
+                                Menu.NONE,
+                                MENU_SHOW_7_DAYS,
+                                Menu.NONE,
+                                R.string.menu_show_7_days_data,
+                            )
+                        show24HoursMenuItem =
+                            menu.add(
+                                Menu.NONE,
+                                MENU_SHOW_24_HOURS,
+                                Menu.NONE,
+                                R.string.menu_show_24_hours_data,
+                            )
+                    }
+
+                    override fun onPrepareMenu(menu: Menu) {
+                        if (
+                            !::show7DaysMenuItem.isInitialized ||
+                                !::show24HoursMenuItem.isInitialized
+                        ) {
+                            return
+                        }
+                        show7DaysMenuItem.setVisible(!viewModel.getShow7Days())
+                        show24HoursMenuItem.setVisible(viewModel.getShow7Days())
+                    }
+
+                    override fun onMenuItemSelected(item: MenuItem): Boolean =
+                        when (item.itemId) {
+                            android.R.id.home -> {
+                                requireActivity().finishAfterTransition()
+                                true
+                            }
+                            MENU_SHOW_7_DAYS -> {
+                                viewModel.updateShow7DaysToggle(true)
+                                true
+                            }
+                            MENU_SHOW_24_HOURS -> {
+                                viewModel.updateShow7DaysToggle(false)
+                                true
+                            }
+                            else -> false
+                        }
+                },
+                viewLifecycleOwner,
+            )
+    }
+
+    private fun onUiStateChanged(uiState: AgentUsageDetailsUiState) {
+        if (preferenceScreen == null) {
+            preferenceScreen = preferenceManager.createPreferenceScreen(requireContext())
+            setPreferenceScreen(preferenceScreen)
+        }
+        preferenceScreen.removeAll()
+
+        when (uiState) {
+            is AgentUsageDetailsUiState.Loading -> {
+                setLoading(true, false)
+            }
+            is AgentUsageDetailsUiState.Failure -> {
+                Log.e(LOG_TAG, "Failed to load agent activities", uiState.throwable)
+                setLoading(false, true)
+            }
+            is AgentUsageDetailsUiState.Success -> {
+                val show7Days = uiState.show7Days
+                if (show7Days) {
+                    addAgentActivityPreferencesForPast7Days(
+                        uiState.agentAccessUiInfos,
+                        preferenceScreen,
+                    )
+                } else {
+                    addAgentActivityPreferencesForPast24Hours(
+                        uiState.agentAccessUiInfos,
+                        preferenceScreen,
+                    )
+                }
+                setLoading(false, true)
+            }
+        }
+    }
+
+    fun addAgentActivityPreferencesForPast24Hours(
+        agentAccessUiInfos: List<AgentAccessUiInfo>,
+        preferenceScreen: PreferenceScreen,
+    ) {
+        val last24Hours =
+            (ZonedDateTime.now(ZoneId.systemDefault()).minusDays(1).toEpochSecond() * 1000L)
+        val category = PreferenceCategory(requireContext())
+        category.title = resources.getString(R.string.agent_activity_timeline_category_title_24h)
+        preferenceScreen.addPreference(category)
+        for (uiInfo: AgentAccessUiInfo in agentAccessUiInfos) {
+            val accessTime = uiInfo.lastAccessTime
+            if (accessTime < last24Hours) {
+                continue
+            }
+            category.addPreference(createAgentActivityPreference(uiInfo))
+        }
+    }
+
+    fun addAgentActivityPreferencesForPast7Days(
+        agentAccessUiInfos: List<AgentAccessUiInfo>,
+        preferenceScreen: PreferenceScreen,
+    ) {
+        val midnightToday =
+            (ZonedDateTime.now(ZoneId.systemDefault())
+                .truncatedTo(ChronoUnit.DAYS)
+                .toEpochSecond() * 1000L)
+        val midnightYesterday =
+            (ZonedDateTime.now(ZoneId.systemDefault())
+                .minusDays(1)
+                .truncatedTo(ChronoUnit.DAYS)
+                .toEpochSecond() * 1000L)
+        var previousAccessDate: Long? = null
+        var category = PreferenceCategory(requireContext())
+
+        for (uiInfo: AgentAccessUiInfo in agentAccessUiInfos) {
+            val accessTime = uiInfo.lastAccessTime
+            val accessDate =
+                ZonedDateTime.ofInstant(Instant.ofEpochMilli(accessTime), ZoneId.systemDefault())
+                    .truncatedTo(ChronoUnit.DAYS)
+                    .toEpochSecond() * 1000L
+            if (previousAccessDate == null || accessDate != previousAccessDate) {
+                val categoryTitle =
+                    if (accessTime > midnightToday) {
+                        resources.getString(R.string.agent_activity_timeline_category_title_today)
+                    } else if (accessTime > midnightYesterday) {
+                        resources.getString(
+                            R.string.agent_activity_timeline_category_title_yesterday
+                        )
+                    } else {
+                        DateFormat.getLongDateFormat(requireContext()).format(accessDate)
+                    }
+                previousAccessDate = accessDate
+
+                category = PreferenceCategory(requireContext())
+                category.title = categoryTitle
+                preferenceScreen.addPreference(category)
+            }
+
+            category.addPreference(createAgentActivityPreference(uiInfo))
+        }
+    }
+
+    fun createAgentActivityPreference(uiInfo: AgentAccessUiInfo): Preference =
+        Preference(requireContext()).apply {
+            title =
+                KotlinUtils.getPackageLabel(
+                    requireActivity().application,
+                    uiInfo.targetPackageName,
+                    Process.myUserHandle(),
+                )
+            icon =
+                KotlinUtils.getBadgedPackageIcon(
+                    requireActivity().application,
+                    uiInfo.targetPackageName,
+                    // TODO: We should use the work profile userHandle in order to get the
+                    // badged icon. The work profile for agents dashboard is not yet
+                    // implemented. Hence using Process.myUserHandle() for now.
+                    Process.myUserHandle(),
+                )
+            summary =
+                resources.getString(
+                    R.string.agent_activity_timeline_activity_summary,
+                    DateFormat.getTimeFormat(requireContext()).format(uiInfo.lastAccessTime),
+                )
+            widgetLayoutResource = R.layout.agent_activity_preference_widget
+            if (uiInfo.interactionUri != null) {
+                onPreferenceClickListener =
+                    Preference.OnPreferenceClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW, uiInfo.interactionUri.toUri())
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                        true
+                    }
+            }
+        }
+
+    companion object {
+        private val LOG_TAG = AgentUsageDetailsFragment::class.java.simpleName
+
+        private const val MENU_SHOW_7_DAYS = Menu.FIRST + 4
+        private const val MENU_SHOW_24_HOURS = Menu.FIRST + 5
+    }
+}
